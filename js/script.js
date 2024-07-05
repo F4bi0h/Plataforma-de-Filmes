2d5402ebb9e743f37216c6f79c99518f3bd286bd
F4BI0H <fabiohn20@gmail.com>
2024-07-04 22:38:09 -0300

    Update project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11.12.1/+esm'
+
 const options = {
   method: 'GET',
   headers: {
@@ -11,28 +13,60 @@ fetch('https://api.themoviedb.org/3/configuration', options)
   .then(response => console.log(response))
   .catch(err => console.error(err));
 
-  let btnPesquisa = document.querySelector('#btnPesquisar');
+
+window.addEventListener('load', () => {
+  Promise.all([
+    fetch('https://api.themoviedb.org/3/movie/popular?language=en-USS&page=1', options),
+    fetch('https://api.themoviedb.org/3/tv/popular?language=pt-BRS&page=1', options)
+  ])
+  .then(async ([moviesResponse, seriesResponse]) => {
+
+    const moviesData = await moviesResponse.json();
+    const seriesData = await seriesResponse.json();
+
+    const combinedResults = [...moviesData.results, ...seriesData.results];
+
+    combinedResults.sort(() => Math.random() - 0.5);
+
+    listarFilmesSeries(combinedResults);
+    console.log(combinedResults)
+  })
+  .catch(err => console.error(err));
+});
+
+
+let btnPesquisa = document.querySelector('#btnPesquisar');
 btnPesquisa.addEventListener('click', event => {
   event.preventDefault();
 
   let valorInput = document.querySelector('#valorPesquisa');
-  console.log(valorInput.value);
 
-  if(valorInput.value != '') {
+  if (valorInput.value != '') {
     fetch(`https://api.themoviedb.org/3/search/movie?query=${valorInput.value}&include_adult=false&language=pt-BR&page=1`, options)
-    .then(response => response.json())
-    .then(response => {
-      if (response.results && response.results.length > 0) {
-        listarFilmesSeries(response.results);
-      } else {
-        alert('Esse filme não consta!');
-      }
-    })
-    .catch(err => console.error(err));
+      .then(response => response.json())
+      .then(response => {
+        if (response.results && response.results.length > 0) {
+          listarFilmesSeries(response.results);
+        } else {
+          Swal.fire({
+            title: 'Error!',
+            text: 'Esse filme não consta no banco de dados!',
+            icon: 'error',
+            confirmButtonText: 'Fechar'
+          });
+        }
+      })
+      .catch(err => console.error(err));
 
     valorInput.value = '';
   } else {
-    alert('Por favor, insira um termo de pesquisa.');
+    Swal.fire({
+      title: 'Error!',
+      text: 'Insira um titulo de pesquisa!',
+      icon: 'error',
+      confirmButtonText: 'Fechar'
+    });
+
     return;
   }
 });
@@ -44,7 +78,7 @@ function listarFilmesSeries(json) {
   lista.innerHTML = '';
 
   json.forEach(item => {
-    criarListaDeFilmesSeries(item.title, urlImg + item.poster_path, item.id);
+    criarListaDeFilmesSeries(item.title || item.original_name, urlImg + item.poster_path, item.id);
   });
 }
 
